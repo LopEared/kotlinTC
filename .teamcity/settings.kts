f59e72b5ce4ae571b623da7be4eb530ba33751d3
LopEared <40773335+LopEared@users.noreply.github.com>
2025-02-04 13:21:34 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -44,3 +44,32 @@ object SubProject2 : Project({
     name = "SubProject_2"
     description = "First Level Subproject_2"
 })
+
+object SubProject1_TestSomeCommands : BuildType({
+    name = "TestSomeCommands"
+    description = "TestSomeCommands"
+
+    params {
+        select("env.TestParam_1", "first_option", label = "SHowLabel", description = "Test of variable choice",
+                options = listOf("first_option", "second_option", "third_option"))
+    }
+
+    steps {
+        script {
+            name = "Create Text File"
+            id = "Create_Text_File"
+            workingDir = "/"
+            scriptContent = "touch self_created_file.txt"
+        }
+        script {
+            name = "Create Second File"
+            id = "Create_Second_File"
+            workingDir = "/"
+            scriptContent = """echo "Test Note for Second reated file %env.TestParam_1%" >  second_created_file_%env.TestParam_1%.txt"""
+        }
+    }
+
+    requirements {
+        equals("teamcity.agent.name", "ip_172.17.0.1")
+    }
+})
